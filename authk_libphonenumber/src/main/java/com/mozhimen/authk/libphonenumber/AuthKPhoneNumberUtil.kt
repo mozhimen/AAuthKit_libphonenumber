cfb75ae1530479de9c69c641621628f62505dcf0
Kolin Zhao & Mozhimen <mozhimen@163.com>
2023-12-24 00:05:19 +0800

    1224

---
+++ authk_libphonenumber/src/main/java/com/mozhimen/authk/libphonenumber/AuthKPhoneNumberUtil.kt
@@ -0,0 +1,39 @@
+package com.mozhimen.authk.libphonenumber
+
+import android.content.Context
+import android.util.Log
+import com.mozhimen.basick.utilk.bases.IUtilK
+import io.michaelrocks.libphonenumber.android.NumberParseException
+import io.michaelrocks.libphonenumber.android.PhoneNumberUtil
+
+/**
+ * @ClassName AuthKPhoneNumberUtil
+ * @Description TODO
+ * @Author Mozhimen / Kolin Zhao
+ * @Date 2023/12/22 23:53
+ * @Version 1.0
+ */
+
+object AuthKPhoneNumberUtil : IUtilK {
+
+    /**
+     * 根据区号判断是否是正确的电话号码
+     * 格式：国家码 86  手机号 +8618800183546
+     * @param phoneNumber :带国家码的电话号码 +8618800183546
+     * @param countryCode :默认国家码 86
+     * return ：true 合法  false：不合法
+     */
+    @JvmStatic
+    fun isPhoneNumberValid(context: Context, phoneNumber: String, countryCode: String): Boolean {
+        Log.d(TAG, "isPhoneNumberValid: $phoneNumber/$countryCode")
+        val phoneUtil: PhoneNumberUtil = PhoneNumberUtil.createInstance(context)
+        try {
+            val numberProto = phoneUtil.parse("+$countryCode$phoneNumber", countryCode)
+            return phoneUtil.isValidNumber(numberProto)
+        } catch (e: NumberParseException) {
+            e.printStackTrace()
+            Log.e(TAG, "isPhoneNumberValid: NumberParseException was thrown: $e")
+        }
+        return false
+    }
+}
